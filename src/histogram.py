5554b182c78ae0d7bc20df03b0defd288b69dc5c
Erwan <erwan.fayolle@gmail.com>
2025-12-10 16:47:24 +0100

    feat: hist done

--- src/histogram.py
+++ src/histogram.py
@@ -3,29 +3,36 @@ import logging
 
 import matplotlib.pyplot as plt
 import pandas as pd
+import math
 from describe import checkInput, readCSV
-
-def plotHistogram(column, column_name):
-	print(f"Plotting histogram for column: {column_name}")
-	return column.dropna().astype(float).values
 
 def main():
 	data = readCSV(checkInput())
+
+	numeric_columns = [col for col in data.columns if (data[col].dtype == 'float64' or data[col].dtype == 'int64') and not data[col].isna().all() and col != 'Index']
+	if len(numeric_columns) == 0:
+		logging.error("No numeric columns found in the dataset to plot histograms.")
+		return
 	
-	fig = plt.figure()
-	for i, column in enumerate(data.columns):
-		if (data[column].dtype == 'float64' or data[column].dtype == 'int64') and data[column].isna().all() == False and column != 'Index':
-			values = plotHistogram(data[column], column)
-			ax = fig.add_subplot(4, 4, i + 1)
-			ax.hist(list(values), bins=10, edgecolor='black')
-			ax.set_title(f'Histogram of {column}')
-			ax.set_xlabel('Value')
-			ax.set_ylabel('Frequency')
+	cols = min(5, len(numeric_columns))
+	rows = math.ceil(len(numeric_columns) / cols)
+	max = rows * cols
+
+	fig, axes = plt.subplots(nrows=rows, ncols=cols, figsize=(15, 9))
+	i = 0
+	axes = axes.flatten()
+	for column in numeric_columns:
+		axes[i].hist(data[column].dropna().astype(float).values, bins=50, color='blue', alpha=0.7)
+		axes[i].set_title(f'Histogram of {column}')
+		axes[i].set_xlabel(column)
+		axes[i].set_ylabel('Frequency')
+		i += 1
+	
+	for j in range(i, max):
+		axes[j].axis('off')
+
+	plt.tight_layout()
 	plt.show()
-	
-
-
-
 
 if "__main__" == __name__:
 	main()
---
+++ src/scatter_plot.py
@@ -0,0 +1,41 @@
+import logging
+# logging.basicConfig(level=logging.DEBUG)
+
+import matplotlib.pyplot as plt
+import pandas as pd
+import math
+from describe import checkInput, readCSV
+
+def main():
+	data = readCSV(checkInput())
+
+	numeric_columns = [col for col in data.columns if (data[col].dtype == 'float64' or data[col].dtype == 'int64') and not data[col].isna().all() and col != 'Index']
+	if len(numeric_columns) == 0:
+		logging.error("No numeric columns found in the dataset to plot histograms.")
+		return
+	
+	
+
+	
+	# cols = min(5, len(numeric_columns))
+	# rows = math.ceil(len(numeric_columns) / cols)
+	# max = rows * cols
+
+	# fig, axes = plt.subplots(nrows=rows, ncols=cols, figsize=(15, 9))
+	# i = 0
+	# axes = axes.flatten()
+	# for column in numeric_columns:
+	# 	axes[i].scatter(data[column].dropna().astype(float).values, data[column].dropna().astype(float).values, color='blue', alpha=1)
+	# 	axes[i].set_title(f'Scatter Plot of {column}')
+	# 	axes[i].set_xlabel(column)
+	# 	axes[i].set_ylabel(column)
+	# 	i += 1
+	
+	# for j in range(i, max):
+	# 	axes[j].axis('off')
+
+	# plt.tight_layout()
+	plt.show()
+
+if "__main__" == __name__:
+	main()
