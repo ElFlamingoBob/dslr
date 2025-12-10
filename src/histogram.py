67b9534df1353e3e5229a6b08bd40e95d792113a
Erwan <erwan.fayolle@gmail.com>
2025-12-10 15:00:14 +0100

    e

--- src/histogram.py
+++ src/histogram.py
@@ -1,36 +1,31 @@
 import logging
 # logging.basicConfig(level=logging.DEBUG)
 
-
 import matplotlib.pyplot as plt
 import pandas as pd
-import sys
-from describe import describeData, checkInput, readCSV
+from describe import checkInput, readCSV
 
 def plotHistogram(column, column_name):
-    print(f"Plotting histogram for column: {column_name}")
-    values = {float(v) for v in column if not pd.isna(v)}
-    plt.hist(list(values), bins=30, edgecolor='black')
-    plt.title(f'Histogram of {column_name}')
-    plt.xlabel('Value')
-    plt.ylabel('Frequency')
-    plt.show()
-    return values
+	print(f"Plotting histogram for column: {column_name}")
+	return column.dropna().astype(float).values
 
 def main():
-    data = readCSV(checkInput())
-    describer = describeData(data)
-    range_ratio = {}
-    # print(describer)
-    # all_values = {}
-    for column in data.columns:
-        if (data[column].dtype == 'float64' or data[column].dtype == 'int64') and data[column].isna().all() == False:
-            range_ratio[column] = (describer.at['75%', column] - describer.at['25%', column]) / (describer.at['max', column] - describer.at['min', column])
-            plotHistogram(data[column], column)
-    print("Range ratios:", range_ratio)
-			
+	data = readCSV(checkInput())
+	
+	fig = plt.figure()
+	for i, column in enumerate(data.columns):
+		if (data[column].dtype == 'float64' or data[column].dtype == 'int64') and data[column].isna().all() == False and column != 'Index':
+			values = plotHistogram(data[column], column)
+			ax = fig.add_subplot(4, 4, i + 1)
+			ax.hist(list(values), bins=10, edgecolor='black')
+			ax.set_title(f'Histogram of {column}')
+			ax.set_xlabel('Value')
+			ax.set_ylabel('Frequency')
+	plt.show()
+	
+
 
 
 
 if "__main__" == __name__:
-    main()
+	main()
